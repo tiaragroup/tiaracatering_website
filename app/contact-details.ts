1f6afbd81e67e3d04e15c1a75644790c9750c35d
Louai Talat Ahmad <Louai@tiaragroup.com>
2026-08-11 11:37:00 +0300

    updated the whatsapp number TG-771

--- app/contact-details.ts
+++ app/contact-details.ts
@@ -2,7 +2,7 @@ export const CALL_NUMBER = "920005600";
 export const CALL_DISPLAY = "9200 05600";
 export const CALL_HREF = `tel:${CALL_NUMBER}`;
 
-export const WHATSAPP_NUMBER = "966920020062";
+export const WHATSAPP_NUMBER = "966114885165";
 export const WHATSAPP_URL = `https://wa.me/${WHATSAPP_NUMBER}`;
 
 export const EMAIL = "info@tiaracatering.com";
