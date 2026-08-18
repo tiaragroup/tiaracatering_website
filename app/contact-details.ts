1c64fae9ae80500cf0e177652d3e5d7b4163a245
Louai Talat Ahmad <Louai@tiaragroup.com>
2026-08-18 10:59:37 +0300

    TG-849

--- app/contact-details.ts
+++ app/contact-details.ts
@@ -1,5 +1,5 @@
-export const CALL_NUMBER = "920005600";
-export const CALL_DISPLAY = "9200 05600";
+export const CALL_NUMBER = "966114885165";
+export const CALL_DISPLAY = "011 488 5165";
 export const CALL_HREF = `tel:${CALL_NUMBER}`;
 
 export const WHATSAPP_NUMBER = "966114885165";
@@ -8,5 +8,5 @@ export const WHATSAPP_URL = `https://wa.me/${WHATSAPP_NUMBER}`;
 export const EMAIL = "info@tiaracatering.com";
 export const EMAIL_HREF = `mailto:${EMAIL}`;
 export const LOCATION_EN = "Abdullah Al-Ahwani, Al-Qirawan District, Riyadh 13531";
-export const LOCATION_AR = "عبدالله الأحواني، حي القيروان، الرياض 13531";
+export const LOCATION_AR = "2744 عبدالله الأهواني, حي القيروان";
 export const LOCATION_MAP_URL = "https://www.google.com/maps/dir//Tiara+Catering,+2744+%D8%B9%D8%A8%D8%AF%D8%A7%D9%84%D9%84%D9%87+%D8%A7%D9%84%D8%A3%D9%87%D9%88%D8%A7%D9%86%D9%8A,+%D8%AD%D9%8A+%D8%A7%D9%84%D9%82%D9%8A%D8%B1%D9%88%D8%A7%D9%86,+RRQA6432%D8%8C+6432,+Riyadh+13531%E2%80%AD/@24.8232638,46.6023878,5652m/data=!3m1!1e3!4m8!4m7!1m0!1m5!1m1!1s0x3e2ee54f19881fe7:0xc7412a82d0876116!2m2!1d46.5950018!2d24.8228728?entry=ttu&g_ep=EgoyMDI2MDgwNS4xIKXMDSoASAFQAw%3D%3D";
